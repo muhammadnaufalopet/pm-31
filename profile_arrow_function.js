0613d26ec3d5262020f638f358c7d7ab754ea520
muhammadnaufalopet <141712648+muhammadnaufalopet@users.noreply.github.com>
2023-10-02 13:32:59 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,6 @@
+const profile = (namaDepan, namaBelakang, email) => {
+  console.log("Nama : " + namaDepan + " "+ namaBelakang)
+  console.log("Email : " + email)
+}
+
+profile("Muhammad","Naufal","bangridzh23@gmail.com")
---
+++ profile_reg_function.js
@@ -0,0 +1,6 @@
+function profile(namaDepan, namaBelakang, email) {
+  console.log("Nama : " + namaDepan + " "+ namaBelakang)
+  console.log("Email : " + email)
+}
+
+profile("Muhammad","Naufal","bangridzh23@gmail.com")
